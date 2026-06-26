6be5268d0b6815ec7ea9f63f09a8a92ec583efe3
csserumaga <alltechissues@gmail.com>
2026-06-26 16:22:59 +0300

    Complete Bitcoin CLI week 1 exercises

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,8 @@
 # Check the total amount in the wallet.
+ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+
+echo "$ADDRESS"
+
+bitcoin-cli -regtest generatetoaddress 101 "$ADDRESS"
+
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
